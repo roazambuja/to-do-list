f758b38d8978435a58c95d70c0ff770abea451e3
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-11-29 19:19:38 -0300

    feat: check if task exists before updating and deleting

--- api/src/controllers/taskController.js
+++ api/src/controllers/taskController.js
@@ -8,7 +8,6 @@ class TaskController {
 
   static async createTask(req, res) {
     try {
-      console.log(req.body);
       const newTask = await task.create(req.body);
       res.status(201).json({ message: "Tarefa cadastrada com sucesso!", task: newTask });
     } catch (erro) {
@@ -19,8 +18,14 @@ class TaskController {
   static async updateTask(req, res) {
     try {
       const id = req.params.id;
-      await task.findByIdAndUpdate(id, { done: true }, { new: true });
-      res.status(200).json({ message: "Tarefa concluída" });
+      let updatedTask = await task.findByIdAndUpdate(id, { done: true }, { new: true });
+      if (updatedTask) {
+        res.status(200).json({ message: "Tarefa concluída" });
+      } else {
+        res.status(404).json({
+          message: "Não foi possível concluir a tarefa, o id informado não foi encontrado.",
+        });
+      }
     } catch (erro) {
       res.status(500).json({ message: `${erro.message} - falha na atualização` });
     }
@@ -29,8 +34,14 @@ class TaskController {
   static async deleteTask(req, res) {
     try {
       const id = req.params.id;
-      await task.findByIdAndDelete(id);
-      res.status(200).json({ message: "Tarefa excluída com sucesso!" });
+      let deletedTask = await task.findByIdAndDelete(id);
+      if (deletedTask) {
+        res.status(200).json({ message: "Tarefa excluída com sucesso!" });
+      } else {
+        res.status(404).json({
+          message: "Não foi possível excluir a tarefa, o id informado não foi encontrado.",
+        });
+      }
     } catch (erro) {
       res.status(500).json({ message: `${erro.message} - falha na exclusão` });
     }
