d98f4d5759ab523c3ef85629122ebdca569e7c8f
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-11-29 21:30:30 -0300

    feat: task icons and style

--- front-end/src/Task/index.tsx
+++ front-end/src/Task/index.tsx
@@ -1,9 +1,14 @@
 import "./task.css";
+import { FaRegTrashAlt, FaCheck } from "react-icons/fa";
 
 function Task() {
   return (
     <div className="task">
       <span>Tarefa</span>
+      <div className="task__icons">
+        <FaRegTrashAlt title="Excluir tarefa" />
+        <FaCheck title="Concluir tarefa" />
+      </div>
     </div>
   );
 }
