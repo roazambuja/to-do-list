30a4d671e59724040bbdb6a592eb2d8cb7f43998
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-12-06 19:36:42 -0300

    feat: config cors

--- api/src/app.js
+++ api/src/app.js
@@ -2,11 +2,20 @@ import express from "express";
 import databaseConnection from "./config/dbConnect.js";
 import dotenv from "dotenv";
 import routes from "./routes/index.js";
+import cors from "cors";
 
 dotenv.config();
 databaseConnection();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
 app.use(express.json());
 routes(app);
 
